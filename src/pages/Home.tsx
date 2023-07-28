9755e5ba4e74ff5a9695d8509bdc59dbe9aaa7a6
Corey Ashford <cjashfor@linux.vnet.ibm.com>
2023-07-28 00:38:46 -0700

    Add navbar and hobo-style nav links

---
+++ src/pages/Home.tsx
@@ -0,0 +1,196 @@
+import { useState } from "react";
+
+import FileUploader from "../components/FileUploader";
+import SocialButtonPage from "../components/SocialButtonPage";
+import IniData from "../IniData";
+import FileDownloader from "../components/FileDownloader";
+import HotButtonData from "../HotButtonData";
+import Alert from "../components/Alert";
+import SocialButtonEditor from "../components/SocialButtonEditor";
+
+import { SocialButtonLoc, HotButtonLoc } from "../ButtonTypes";
+import {
+  loadSocialButtonData,
+  storeSocialButtonData,
+} from "../utils/socialButtonDataUtils";
+import {
+  linkHotButtonToSocialButton,
+  onLinkedHotButtons,
+} from "../utils/hotButtonDataUtils";
+import copyString from "../utils/copyString";
+import copyIniData from "../utils/copyIniData";
+
+import { Container } from "react-bootstrap";
+
+//import dumpHash from "./utils/dumpHash";
+
+const pageNums = Array.from(Array(10), (_, i) => i + 1);
+
+const Home = () => {
+  const [iniData, setIniData] = useState<IniData>({});
+  const [fileName, setFileName] = useState<string>("");
+  const [editSocialButtonLoc, setEditSocialButtonLoc] =
+    useState<SocialButtonLoc>({ pageNum: 0, buttonNum: 0 });
+  const [showSocialButtonEditorModal, setShowSocialButtonEditorModal] =
+    useState<boolean>(false);
+
+  const [srcSocialButtonLoc, setSrcSocialButtonLoc] = useState<SocialButtonLoc>(
+    {
+      pageNum: -1,
+      buttonNum: -1,
+    }
+  );
+
+  const [dstSocialButtonLoc, setDstSocialButtonLoc] = useState<SocialButtonLoc>(
+    {
+      pageNum: -1,
+      buttonNum: -1,
+    }
+  );
+
+  const performAction = (
+    srcButton: SocialButtonLoc,
+    dstButton: SocialButtonLoc
+  ) => {
+    let linkedSrcHotButtons: HotButtonData[] = [];
+    let linkedDstHotButtons: HotButtonData[] = [];
+
+    console.log(
+      "Action src: " +
+        JSON.stringify(srcButton) +
+        "  dst: " +
+        JSON.stringify(dstButton)
+    );
+    onLinkedHotButtons(
+      srcButton,
+      (button: HotButtonLoc, suffix: string) => {
+        linkedSrcHotButtons.push({ hotButtonLoc: button, suffix: suffix });
+      },
+      iniData
+    );
+    onLinkedHotButtons(
+      dstButton,
+      (button: HotButtonLoc, suffix: string) => {
+        linkedDstHotButtons.push({ hotButtonLoc: button, suffix: suffix });
+      },
+      iniData
+    );
+
+    // swap the social key data
+
+    const newIniData = copyIniData(iniData);
+
+    const srcButtonData = loadSocialButtonData(srcButton, newIniData);
+    //console.log("src: " + JSON.stringify(srcButtonData));
+    const dstButtonData = loadSocialButtonData(dstButton, newIniData);
+
+    storeSocialButtonData(srcButton, dstButtonData, newIniData);
+    storeSocialButtonData(dstButton, srcButtonData, newIniData);
+
+    // Fix the hot buttons
+    //console.log("linkedSrctHotButtons: " + JSON.stringify(linkedSrcHotButtons));
+    for (let linkedSrcHotButton of linkedSrcHotButtons) {
+      /*
+      console.log(
+        "linking " +
+          JSON.stringify(linkedSrcHotButton) +
+          " to " +
+          JSON.stringify(dstButton)
+      );
+      */
+      linkHotButtonToSocialButton(linkedSrcHotButton, dstButton, newIniData);
+    }
+    //console.log("linkedDstHotButtons: " + JSON.stringify(linkedDstHotButtons));
+    for (let linkedDstHotButton of linkedDstHotButtons) {
+      /*
+      console.log(
+        "linking " +
+          JSON.stringify(linkedDstHotButton) +
+          " to " +
+          JSON.stringify(srcButton)
+      );
+      */
+      linkHotButtonToSocialButton(linkedDstHotButton, srcButton, newIniData);
+    }
+
+    setIniData(newIniData);
+  };
+
+  // Drop onto = Destination
+  const handleDrop = (buttonLoc: SocialButtonLoc) => {
+    setDstSocialButtonLoc(() => {
+      if (srcSocialButtonLoc.pageNum !== -1) {
+        // This is the second event, so we know that both the source and destination are set
+        performAction(srcSocialButtonLoc, buttonLoc);
+        setDstSocialButtonLoc({ pageNum: -1, buttonNum: -1 });
+      }
+      return buttonLoc;
+    });
+  };
+
+  // Dropped = Source
+  const handleDragEnd = (buttonLoc: SocialButtonLoc) => {
+    setSrcSocialButtonLoc(() => {
+      if (dstSocialButtonLoc.pageNum !== -1) {
+        // This is the second event, so we know that both the source and destination are set
+        performAction(buttonLoc, dstSocialButtonLoc);
+        setSrcSocialButtonLoc({ pageNum: -1, buttonNum: -1 });
+      }
+      return buttonLoc;
+    });
+  };
+
+  //
+  const handleDoubleClick = (buttonLoc: SocialButtonLoc) => {
+    setEditSocialButtonLoc(buttonLoc);
+    setShowSocialButtonEditorModal(true);
+  };
+
+  const fileNameHandler = (name: string) => {
+    const newName: string = copyString(name);
+    setFileName(newName);
+  };
+
+  /*
+  const openExternalLinkInNewTab = (url: string) => {
+    // Create a new window/tab with the external link
+    const newWindow = window.open(url, "_blank");
+    // Add noopener and noreferrer attributes to improve security
+    if (newWindow) newWindow.opener = null;
+  };
+*/
+  return (
+    <>
+      <Container fluid className="p-3">
+        <FileUploader onIniData={setIniData} onFileName={fileNameHandler} />
+        <table>
+          <tbody>
+            <tr key={1}>
+              {pageNums.map((pageNum) => (
+                <td key={pageNum}>
+                  <SocialButtonPage
+                    iniData={iniData}
+                    pageNum={pageNum}
+                    onDrop={handleDrop}
+                    onDragEnd={handleDragEnd}
+                    onDoubleClick={handleDoubleClick}
+                  />
+                </td>
+              ))}
+            </tr>
+          </tbody>
+        </table>
+        <Alert message="Make a backup copy of your original .ini file before using the one created here" />
+        <FileDownloader iniData={iniData} fileName={fileName} />
+        <SocialButtonEditor
+          iniData={iniData}
+          buttonLoc={editSocialButtonLoc}
+          showModal={showSocialButtonEditorModal}
+          onHide={() => setShowSocialButtonEditorModal(false)}
+        />
+      </Container>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/pages/Privacy.tsx
@@ -0,0 +1,15 @@
+const Privacy = () => {
+  return (
+    <>
+      <h4>Privacy and Security</h4>
+      <p>
+        This app runs completely locally in your web browser. No data of any
+        kind will be transferred off of your computer. This will never change.
+        Also it makes the development of the app easier, and requires no hosting
+        fees.
+      </p>
+    </>
+  );
+};
+
+export default Privacy;
