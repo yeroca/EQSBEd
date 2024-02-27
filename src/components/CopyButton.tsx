cc7549a1d3b3392e5fe03383dd88b264c52c4319
Corey Ashford <cjashfor@linux.vnet.ibm.com>
2024-02-26 21:54:12 -0800

    switch from using alert to a briefly appearing tooltip on the Copy button

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useState, useRef } from "react";
+import { Button, Overlay, Tooltip } from "react-bootstrap";
 import clipboardCopy from "clipboard-copy";
 
 interface CopyButtonProps {
@@ -7,18 +7,39 @@ interface CopyButtonProps {
 }
 
 const CopyButton: React.FC<CopyButtonProps> = ({ jsonString }) => {
+  const [showTooltip, setShowTooltip] = useState(false);
+  const buttonRef = useRef(null);
+
   const handleCopy = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     event.preventDefault();
     clipboardCopy(jsonString);
-    alert('"' + jsonString + '" copied to clipboard!');
+    setShowTooltip(true);
+
+    // Hide the tooltip after 1 second
+    setTimeout(() => {
+      setShowTooltip(false);
+    }, 1000);
   };
 
   return (
-    <Button variant="primary" onClick={handleCopy}>
-      Copy button
-    </Button>
+    <>
+      <Button ref={buttonRef} variant="primary" onClick={handleCopy}>
+        Copy button
+      </Button>
+      <Overlay
+        target={buttonRef.current || document.body}
+        show={showTooltip}
+        placement="bottom"
+      >
+        {(props) => (
+          <Tooltip id="overlay-example" {...props}>
+            Copied button to clipboard!
+          </Tooltip>
+        )}
+      </Overlay>
+    </>
   );
 };
 
